02f4528737488609ffa321726991e14cfb7809fc
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2021-12-27 12:15:31 +0530

    #451

---
+++ leetcode/AddStrings.py
@@ -0,0 +1,12 @@
+# Leetcode
+# 415. Add Strings
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:      
+        return str( self.covert_to_unicode(num1) + self.covert_to_unicode(num2))
+    
+    def covert_to_unicode(self, string):
+        result = 0
+        for num in string:
+            result = result * 10 + (ord(num)-ord('0'))
+        return result
