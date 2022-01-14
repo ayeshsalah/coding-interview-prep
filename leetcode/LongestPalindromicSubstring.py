f16dc7b8373d64adb9871e9c5202b7e6a9e2a387
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2022-01-14 22:52:43 +0530

    #5

--- leetcode/LongestPalindromicSubstring.py
+++ leetcode/LongestPalindromicSubstring.py
@@ -5,25 +5,26 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         result = ""
         result_len = 0
-
         for i in range(len(s)):
-            # odd 
+            # odd palindromes
             left, right = i, i 
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 # check if length of current palindrome is greater then stored plaindrome 
-                if (right - left +1) > result_len:
-                    result = s[left:right+1]
-                    result_len = right - left +1
+                temp = s[left:right+1]
+                if len(temp) > result_len:
+                    result = temp
+                    result_len = len(temp)
                 left -= 1
                 right += 1
-                
-            # even 
-            left, right = i, i + 1
+                    
+            # right palindromes
+            left, right = i, i+1
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 # check if length of current palindrome is greater then stored plaindrome 
-                if (right - left +1) > result_len:
-                    result = s[left:right+1]
-                    result_len = right - left +1
+                temp = s[left:right+1]
+                if len(temp) > result_len:
+                    result = temp
+                    result_len = len(temp)
                 left -= 1
                 right += 1
         return result
