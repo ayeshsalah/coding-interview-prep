04b836991024de5790b5071c2e33710fcf0dec42
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2022-02-13 16:54:09 +0530

    Graph Impl

---
+++ DS implementations/Graph/bfs_queue.py
@@ -0,0 +1,29 @@
+# Leetcode 1971
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        
+        adjacency_list = [[] for _ in range(n)]
+        for a, b in edges:
+            adjacency_list[a].append(b)
+            adjacency_list[b].append(a)
+        
+        queue = collections.deque([start])
+        seen = set([start])
+        
+        while queue:
+            # Get the current node.
+            node = queue.popleft()
+            
+            # Check if we have reached the target node.
+            if node == end:
+                return True
+            
+            # Add all neighbors to the queue.
+            for neighbor in adjacency_list[node]:
+                # Check if neighbor has been added to the queue before.
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    queue.append(neighbor)
+        
+        # Our queue is empty and we did not reach the end node.
+        return False
