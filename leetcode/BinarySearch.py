06c01d24ff1e4a8101631421e149fb0d7b57adf5
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-01-14 00:13:39 +0530

    Updated #704

--- leetcode/BinarySearch.py
+++ leetcode/BinarySearch.py
@@ -7,7 +7,9 @@ class Solution:
         left = 0
         right = len(nums)-1
         while left<=right:
-            middle = left + ((right-left)//2) # (l + r) // 2 can lead to overflow
+            # (l + r) // 2 can lead to overflow
+            # Find the distance between the 2 pointers, divide it by 2 and add the left index to the result, to find the mid point.
+            middle = left + ((right-left)//2) 
             if nums[middle] < target:
                 left = middle + 1
             elif nums[middle] > target:
