3b7823d9ea051d9bf37f1cdb0444bef33c099f51
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-01-15 20:02:06 +0530

    Updating leetcode #121

--- leetcode/BestTimeToBuyAndSellStock.py
+++ leetcode/BestTimeToBuyAndSellStock.py
@@ -10,3 +10,15 @@ class Solution:
             max_profit = max(max_profit, price-min_price) # calculate profit at every iteration and store the highest
             min_price= min(min_price, price) #find lowest price
         return max_profit
+
+#### Addtional Solution ####
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        max_profit = 0
+        for right in range(1, len(prices)):
+            if prices[right] < prices[left]:
+                left = right
+            max_profit = max(max_profit, (prices[right] - prices[left]))
+        return max_profit
