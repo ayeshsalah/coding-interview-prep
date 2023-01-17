68442977297495217086ee5666e05fe50aab566a
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-01-18 00:19:52 +0530

    Updated Leetcode #424

--- leetcode/LongestRepeatingCharacterReplacement.py
+++ leetcode/LongestRepeatingCharacterReplacement.py
@@ -1,8 +1,9 @@
 # Leetcode
 # 424. Longest Repeating Character Replacement
 # https://www.youtube.com/watch?v=gqXU1UyA8pk
+# https://leetcode.com/problems/longest-repeating-character-replacement/solutions/765776/python-two-pointers-process-for-coding-interviews/
 
-#TODO add explanation comments 
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
@@ -18,3 +19,24 @@ class Solution:
                 left += 1
             result = max(result, right-left+1)
         return result
+
+#### Additional Solution ####
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        max_len = 0
+        hashmap = {}
+        for right in range(0, len(s)):
+            if s[right] in hashmap.keys():
+                hashmap[s[right]] += 1
+            else:
+                hashmap[s[right]] = 1
+            max_freq = max(hashmap.values())
+            if (right-left+1) - max_freq <= k:
+                max_len = max(max_len, (right-left+1))
+            else:
+                hashmap[s[left]] -= 1
+                left += 1
+        return max_len
