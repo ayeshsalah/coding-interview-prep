cdb075245afa25e68551d1d1c69eda1217343c01
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-08-01 23:46:35 +0530

    Updated solution for #169

--- leetcode/MajorityElement.py
+++ leetcode/MajorityElement.py
@@ -2,7 +2,17 @@
 # 169. Majority Element
 # Boyer-Moore Majority Vote Algorithm
 # https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+# https://leetcode.com/problems/majority-element/solutions/3676530/3-method-s-beats-100-c-java-python-beginner-friendly/?envType=study-plan-v2&envId=top-interview-150
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return collections.Counter(nums).most_common()[0][0]
+        candidate = nums[0]
+        count = 1
+        for n in nums[1:]:
+            if count == 0:
+                candidate = n            
+            if n == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
