574b71fd3bc4d2f13ceac4bf9343a2e077bdde90
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-08-27 14:37:07 +0530

    Improved soluton for #48

--- leetcode/RotateImage.py
+++ leetcode/RotateImage.py
@@ -7,5 +7,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse() # reverse matrix
-        matrix[:] = zip(*matrix) # transpose matrix and assign back to same object
+        left = 0 
+        right = len(matrix)-1
+        while left<right:
+            # i will be used to iterate thru all the rows
+            for i in range(right-left):
+                top = left
+                bottom = right
+                # save top left      
+                top_left = matrix[top][left+i]
+                # move bottom left into top left
+                matrix[top][left+i] = matrix[bottom-i][left]
+                # move bottom right to bottom left
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+                # move top right to bottom right
+                matrix[bottom][right-i] = matrix[top+i][right]
+                # move saved top_left to top right
+                matrix[top+i][right] = top_left
+            left +=1
+            right -= 1
