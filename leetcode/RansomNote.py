a41865e9a009765f51c1959f42c69626e0ed76f2
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-08-28 23:32:36 +0530

    Added alternate solution for #383

--- leetcode/RansomNote.py
+++ leetcode/RansomNote.py
@@ -2,6 +2,26 @@
 # 383. Ransom Note
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_hashmap = {}
+        magazine_hashmap = {}
+
+        for i in ransomNote:
+            ransom_hashmap[i] = ransom_hashmap.get(i, 0) + 1
+
+        for i in magazine:
+            if i in ransom_hashmap:
+                magazine_hashmap[i] = magazine_hashmap.get(i, 0) + 1
+
+        for key, value in ransom_hashmap.items():
+            if key not in magazine_hashmap:
+                return False
+            elif magazine_hashmap[key] < value:
+                return False
+        return True
+
+##########################################################################
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         if len(magazine) < len(ransomNote):
             return False
         
@@ -13,23 +33,24 @@ class Solution:
                 return False
         return True
     
-####################################################################    
-#         #Alternate solution            
-#         counter = collections.Counter(magazine)
-#         ransomNote = list(ransomNote)
+##########################################################################
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:  
+        counter = collections.Counter(magazine)
+        ransomNote = list(ransomNote)
         
-#         for alpha in list(ransomNote):
-#             ransomNote.remove(alpha)
-#             alpha_count = counter.get(alpha, -1)
-#             if alpha_count > 1:
-#                 counter[alpha] = counter.get(alpha) - 1 
-#             elif alpha_count == 1:
-#                 counter.pop(alpha)
-#             else:
-#                 return False
+        for alpha in list(ransomNote):
+            ransomNote.remove(alpha)
+            alpha_count = counter.get(alpha, -1)
+            if alpha_count > 1:
+                counter[alpha] = counter.get(alpha) - 1 
+            elif alpha_count == 1:
+                counter.pop(alpha)
+            else:
+                return False
             
-#         if not ransomNote:
-#             return True
-#         else:
-#             return False
+        if not ransomNote:
+            return True
+        else:
+            return False
             
