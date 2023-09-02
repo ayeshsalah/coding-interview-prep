174f0c66dc4f2b7bd6d9e15ed012abf8421b034e
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-09-02 22:57:43 +0530

    added extra solution for #242

--- leetcode/ValidAnagram.py
+++ leetcode/ValidAnagram.py
@@ -8,3 +8,13 @@ class Solution:
         
         return s_counter == t_counter
         
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+        for c in s:
+            s_dict[c] = s_dict.get(c, 0) + 1
+        for c in t:
+            t_dict[c] = t_dict.get(c, 0) + 1
+        return s_dict == t_dict
