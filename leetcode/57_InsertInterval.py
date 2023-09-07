4a0b2943319105df520dfcc0fed1e50d7be2efbc
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-09-07 23:52:08 +0530

    Solution for #57

--- leetcode/57_InsertInterval.py
+++ leetcode/57_InsertInterval.py
@@ -8,19 +8,19 @@ class Solution:
         result = []
         for i in range(len(intervals)):
             # check if newInterval is smaller then current interval
-            # if it smaller, append to resutl and return result + remaining valuesof intervals 
+            # if it smaller, append to result and return result + remaining valuesof intervals 
             if newInterval[1] < intervals[i][0]:
                 result.append(newInterval)
                 return result + intervals[i:]
             # check if the newInterval is bigger than the current interval, if yes, append the current interval to result
             elif newInterval[0] > intervals[i][1]:
                 result.append(intervals[i])
-                # not appending to result to check if it overlaps with follwing intervals
-            # if both condition do not satisify then newInterval is overlapping with current interval
+                # not appending to result to check if it overlaps with following intervals
+            # if both conditions do not satisify then newInterval is overlapping with current interval
             # update the newInterval, i.e., merge the intervals.
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-                # not appending to result to check if it overlaps with follwing intervals
+                # not appending to result to check if it overlaps with following intervals
         # if we reach the end of the loop, append the newInterval
         result.append(newInterval)
         return result
