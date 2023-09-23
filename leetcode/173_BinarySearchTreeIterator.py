d16208ae55375fa40e0a97f04125a223d228e943
Ayesh Salahuddin <ayeshsalah@users.noreply.github.com>
2023-09-23 15:30:35 +0530

    Solution #173

--- leetcode/173_BinarySearchTreeIterator.py
+++ leetcode/173_BinarySearchTreeIterator.py
@@ -1,33 +1,36 @@
 # Leetcode
 # 173. Binary Search Tree Iterator
-# https://www.youtube.com/watch?v=D2jMcmxU4bs
+# https://www.youtube.com/watch?v=RXy5RzGF5wo
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
         self.stack = []
+        # add all left nodes to stack
         while root:
             self.stack.append(root)
             root = root.left
 
-    # returns the next smallest number
     def next(self) -> int:
+        # since next() call will always be valid, just pop stack and return val.
         node = self.stack.pop()
+        # append right node to stack
         right_node = node.right
+        # if right node is valid, then add any left nodes to stack.
         while right_node:
             self.stack.append(right_node)
             right_node = right_node.left
         return node.val      
         
-    # returns boolean, whether we have a next smallest number
+    # returns boolean, whether we have a next element
     def hasNext(self) -> bool:
         return len(self.stack) > 0
 
